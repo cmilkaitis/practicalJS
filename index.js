be5b724ed97b1acc3b14b75369da9b794585482c
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-04-30 16:58:56 -0400

    version 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//VERSION 1
 
     //save items to a list
 var todo = ["item 1", "item 2", "item 3"];
@@ -11,3 +12,24 @@ todo[0] = "item 1 updated";
     //remove items from the list
 todo.splice(0,1);
 
+
+//VERSION 2
+
+function displayTodos(){
+    console.log("My todos:", todos);
+}
+
+function addTodo(item){
+    todos.push(item);
+    displayTodos();
+}
+
+function changeTodo(position, newValue){
+    todo[position] = newValue;
+    displayTodos();
+}
+
+function deleteTodo(position){
+    todo.splice(position, 1)
+    displayTodos();
+}
