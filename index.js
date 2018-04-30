b82358aca997c8985c3ccea36f0571a4fd02721c
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-04-30 16:35:11 -0400

    version 1

---
+++ index.js
@@ -0,0 +1,13 @@
+
+    //save items to a list
+var todo = ["item 1", "item 2", "item 3"];
+
+    //add to the list
+todo.push("item 4");
+
+    //update items on the list
+todo[0] = "item 1 updated";
+
+    //remove items from the list
+todo.splice(0,1);
+
